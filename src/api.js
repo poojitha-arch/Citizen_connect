fa44de783a3bf190c13660b67b36fad13751d41f
poojitha-arch <poojitha27.ai@gmail.com>
2026-04-28 02:18:58 +0530

    chore: update API_BASE_URL to point to production environment

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
+const API_BASE_URL = "https://springbootdeployment-production-f5dc.up.railway.app";
 export default API_BASE_URL;
